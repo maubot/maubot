2d95b54758e436cd7feb3da1ef7a4b18a7b7fcce
Tulir Asokan <tulir@maunium.net>
2018-10-17 01:31:12 +0300

    Remove event proxy class

--- maubot/__init__.py
+++ maubot/__init__.py
@@ -1,4 +1,3 @@
 from .plugin_base import Plugin
 from .command_spec import CommandSpec, Command, PassiveCommand, Argument
-from .event import FakeEvent as Event
 from .client import MaubotMatrixClient as Client
