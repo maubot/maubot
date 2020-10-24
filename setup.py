6d8a05feaf2c9d9b4f6c7a2785ee9e8352110dde
Tulir Asokan <tulir@maunium.net>
2020-10-24 15:13:50 +0300

    Update mautrix-python

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     entry_points="""
         [console_scripts]
