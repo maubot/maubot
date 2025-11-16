f671824f5b9116840127f8f9a9576fad393c6f8a
Tulir Asokan <tulir@maunium.net>
2025-11-17 00:26:41 +0200

    Update linters, CI and python version

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     entry_points="""
         [console_scripts]
